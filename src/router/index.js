af499ebd79c1ffbfac5294ec67311ff12aa1d4a5
santu <j6458221@yahoo.com.tw>
2024-02-27 13:49:28 +0800

    前後台路由建置完成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
+  // 前台
   {
     path: '/',
     component: () => import('../views/FrontView.vue'),
@@ -14,13 +15,31 @@ const routes = [
         path: 'about',
         name: 'About',
         component: () => import('../views/AboutView.vue')
+      },
+      {
+        path: 'products',
+        component: () => import('../views/ProductsView.vue')
+      },
+      {
+        path: 'cart',
+        component: () => import('../views/CartView.vue')
       }
     ]
   },
+
+  // 後台
   {
     path: '/admin',
     component: () => import('../views/dashboard/DashboardView.vue'),
     children: [
+      {
+        path: 'products',
+        component: () => import('../views/dashboard/ProductsView.vue')
+      },
+      {
+        path: 'order',
+        component: () => import('../views/dashboard/OrderView.vue')
+      }
     ]
   }
 ]
--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -1,4 +1,11 @@
 <template>
   <h2>這是前台</h2>
+  <nav>
+    <RouterLink to="/">Home</RouterLink> |
+    <RouterLink to="/about">About</RouterLink> |
+    <RouterLink to="/products">產品列表</RouterLink> |
+    <RouterLink to="/cart">購物車列表</RouterLink> |
+    <RouterLink to="/admin">後台</RouterLink>
+  </nav>
   <RouterView></RouterView>
 </template>
